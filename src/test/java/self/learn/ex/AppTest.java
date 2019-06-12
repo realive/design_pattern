16b408eb75beed7286f66523a92ab2ead6150d34
realive <realive@live.cn>
2019-06-12 19:33:46 +0800

    init

---
+++ src/test/java/self/learn/ex/AppTest.java
@@ -0,0 +1,20 @@
+package self.learn.ex;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
